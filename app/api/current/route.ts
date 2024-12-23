367ef83942754500e58f4550944eb9e3b2b57c5f
Arunbalaji07 <stararunbalaji12@gmail.com>
2024-12-23 08:25:19 +0530

    Implemented server auth in /api/current and added status in response.

--- app/api/current/route.ts
+++ app/api/current/route.ts
@@ -1,12 +1,27 @@
 import {NextResponse} from "next/server";
 
-import { serverAuth } from "@/lib/auth";
+import {auth} from "@/auth";
 
 export async function GET() {
   try {
-    const { currentUser } = await serverAuth();
+    const session = await auth()
+
+    if(!session?.user?.email) {
+      throw new Error("Not signed in!")
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+    });
+
+    if (!currentUser) {
+      throw new Error("Not signed in");
+    }
+
     console.log("Current user : " +currentUser)
-    return NextResponse.json(currentUser);
+    return NextResponse.json(currentUser, {status: 200});
   } catch (err) {
     console.log(err);
     // return res.status(400).end();
